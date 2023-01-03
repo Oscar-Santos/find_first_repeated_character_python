f6a015d75dc1f173c1069256e02c773b6a265123
Oscar Santos <oscar303data@gmail.com>
2023-01-03 10:51:50 -0700

    finish writing pesudocode 1

--- find_1_repeated_char.py
+++ find_1_repeated_char.py
@@ -1,3 +1,13 @@
 comm='''
 Tech Challenge in Python: Find the first repeated character in a string
+
+Pseudocode 1:
+1) find the character that occurs more than once
+2) example:
+    2.1) input = 'geek app'; output = e
+    2.2) input = 'hello cool guys'; output = l
+3) run 2 nested loops
+4) traverse from left to right to check if a letter / character repeats or not
+5) if letter / character repeats, increment count of repeating characters
+6) when the count becomes K, return the character
 '''
